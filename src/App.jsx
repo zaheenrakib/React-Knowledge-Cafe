586bd5f8375f88d93baaff4547702139bd1dd835
Zaheen Rakib <rakibzaheen@gmail.com>
2024-03-15 16:31:32 +0600

    Create fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl bg-red-400'>Knowledge Cafe</h1>
- 
+
+    <Header></Header>
+
+    <Blogs></Blogs>
+
+
     </>
   )
 }
