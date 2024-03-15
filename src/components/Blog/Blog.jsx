5c8f5deba4fcd73e8895df365e2439795f5dfb45
Zaheen Rakib <rakibzaheen@gmail.com>
2024-03-15 17:55:27 +0600

    Set Layout

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({blog}) => {
+    console.log(blog);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Blog.PropTypes = {
+    blog: PropTypes.object.isRequired
+}
+
+export default Blog;
